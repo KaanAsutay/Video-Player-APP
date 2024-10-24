50138c61e24b7431b9d5256a1298c14188e33cdc
Kaan <kaanasutay10@gmail.com>
2024-10-25 00:50:25 +0300

    add videoModels

---
+++ backend/models/video.js
@@ -0,0 +1,28 @@
+"use strict"
+/* -------------------------------------------------------
+    NODEJS EXPRESS | Video Player
+------------------------------------------------------- */
+const mongoose = require('mongoose');
+
+const VideoSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        trim: true,
+        required: true
+    },
+    description: {
+        type: String,
+        trim: true,
+    },
+    videoUrl: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    filename: {
+        type: String,
+        trim: true,
+    }
+}, {timestamps: true})
+
+module.exports = mongoose.model('MyVideos', VideoSchema)
