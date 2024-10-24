0c50581def26075a797791883a2720a25ebfe633
Kaan <kaanasutay10@gmail.com>
2024-10-25 01:11:57 +0300

    create controllers and connect routes

--- backend/routes/video.js
+++ backend/routes/video.js
@@ -3,11 +3,10 @@
     NODEJS EXPRESS | Video Player
 ------------------------------------------------------- */
 const router = require('express').Router()
+const { addVideo, getAllVideos } = require('../controllers/video')
 const { videoUpload } = require('../middlewares/videoUpload')
 
-router.post('/upload', videoUpload.single('video'), 
-    (req, res) => {
-        res.send({message: 'Video Uploaded'})
-})
+router.post('/upload', videoUpload.single('video'), addVideo)
+    .get('/videos', getAllVideos)
 
 module.exports = router
