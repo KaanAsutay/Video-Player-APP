ab9258d41a3d590d2f6f79489fa4069cff747a15
Kaan <kaanasutay10@gmail.com>
2024-10-25 23:45:44 +0300

    add get videos

--- frontend/src/Context/global.js
+++ frontend/src/Context/global.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const GlobalContext =  React.createContext()
 
 const reducer = (state, action) => {
     switch(action.type) {
-        
+
     }
 
     return state
@@ -18,6 +18,22 @@ export const GlobalProvider = ({children}) => {
     }
 
     const [state, dispatch] = React.useReducer(reducer, initialState)
+
+    // get videos
+    const getAllVideos = async () => {
+        try {
+            const res = await fetch('http://localhost:8000/api/videos');
+            const data = await res.json()
+
+            console.log(data.videos)
+        } catch (error) {
+            
+        }
+    }
+
+    useEffect(() => {
+        getAllVideos()
+    }, [])
 
     return (
         <GlobalContext.Provider value={'HELLO WORLD'}>
