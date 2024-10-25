92278cee4707fe4f5e37dcdc71d80aaf6b2181df
Kaan <kaanasutay10@gmail.com>
2024-10-26 00:29:53 +0300

    add some AppStyled and connect browserRouter, routes and route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,39 @@
+import styled from "styled-components";
 import Videos from "./Components/Videos";
 import { useGlobalContext } from "./Context/global";
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   const g = useGlobalContext()
   console.log(g)
-  
+
   return (
-    <div className="App">
-      <Videos />
-    </div>
+    <BrowserRouter>
+      <AppStyled className="App">
+        <h1>Video Uploader</h1>
+        <Routes>
+          <Route path="/" element={<Videos />} />
+          <Route path="/videos/:id" element={<Videos />} />
+        </Routes>
+        <Videos />
+      </AppStyled>
+    </BrowserRouter>
   );
 }
+
+const AppStyled = styled.div`
+  padding: 3rem 18rem;
+  
+  h1{
+    color: #fff;
+    background: linear-gradient(to right, #00b894 40%,#705DF2 );
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    display: inline-block;
+    position: relative;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+`;
 
 export default App;
