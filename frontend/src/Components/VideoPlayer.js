2d8d2c6b5faf9d4cb840af8838eb1370d29668bf
Kaan <kaanasutay10@gmail.com>
2024-10-26 01:42:44 +0300

    ou can handle player events here, for example

--- frontend/src/Components/VideoPlayer.js
+++ frontend/src/Components/VideoPlayer.js
@@ -1,17 +1,44 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { useParams } from 'react-router-dom'
 import { useGlobalContext } from '../Context/global'
+import styled from 'styled-components'
+import VideoJS from './VideoJS'
+import videojs from 'video.js'
 
 function VideoPlayer() {
 
     const {id} = useParams()
     const {videos} = useGlobalContext()
 
-    //refs
+     //refs
+     const videoConRef = useRef(null); 
+     const playerRef = React.useRef(null);
+ 
+ 
+     const handlePlayerReady = (player) => {
+         playerRef.current = player;
+ 
+    // You can handle player events here, for example:
+         player.on('waiting', () => {
+         videojs.log('player is waiting');
+         });
+ 
+         player.on('dispose', () => {
+         videojs.log('player will dispose');
+         });
+     };
 
   return (
-    <div>VideoPlayer</div>
+    <VideoPlayerStyled>
+        <div className='video-container' ref={videoConRef}>
+            <VideoJS options={videoOptions} onReady={handlePlayerReady} />
+        </div>
+    </VideoPlayerStyled>
   )
 }
+
+const VideoPlayerStyled = styled.div`
+
+`;
 
 export default VideoPlayer
