1f0af48ff78344374b0b6a3a94811967055a2e4d
Kaan <kaanasutay10@gmail.com>
2024-10-28 00:21:41 +0300

    add button styled and connect with Upload.js

--- frontend/src/Components/Button.js
+++ frontend/src/Components/Button.js
@@ -16,7 +16,18 @@ function Button(name, onClick, icon, bg, type, disabled) {
 }
 
 const ButtonStyled = styled.button`
-
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    padding: 0.8rem 1.5rem;
+    border-radius: 7px;
+    cursor: pointer;
+    transition: all .4s ease;
+    color: rgba(255,255,255,0.8);
+    font-weight: 600;
+    &:hover{
+        color: rgba(255,255,255, 1);
+    }
 `;
 
 export default Button
--- frontend/src/Components/Upload.js
+++ frontend/src/Components/Upload.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import Button from './Button';
 
 function Upload() {
     const [video, setVideo] = React.useState(null);
