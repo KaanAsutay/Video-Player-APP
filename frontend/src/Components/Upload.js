823cb7fe2b5ebb9d022163cb9fabb3417b2acb9c
Kaan <kaanasutay10@gmail.com>
2024-10-28 00:29:00 +0300

    add upload styled

--- frontend/src/Components/Upload.js
+++ frontend/src/Components/Upload.js
@@ -25,7 +25,7 @@ function Upload() {
     return (
         <UploadStyled>
             <h2>Upload Video</h2>
-            <form onSubmit={handleUpload} action="api/upload" method='POST' encType='multipart/form-data'>
+            <form  action="api/upload" method='POST' encType='multipart/form-data'>
                 <div className="input-control">
                     <label htmlFor="title">Title</label>
                     <input 
@@ -86,6 +86,76 @@ function Upload() {
 const UploadStyled = styled.div`
     position: fixed;
     z-index: 5;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 40%;
+    background: #262626;  
+    padding: 2rem; 
+    border-radius: 15px;
+    box-shadow: 3px 5px 30px rgba(255,255,255,0.1);
+    h2{
+        color: #fff;
+        text-align: center;
+        font-size: 2rem;
+        margin-bottom: 2rem;
+        opacity: 0.9;
+    }
+    form{
+        display: flex;
+        flex-direction: column;
+        gap: 2rem;
+        .input-control{
+            display: flex;
+            flex-direction: column;
+            input, textarea{
+                padding: .8rem 1rem;
+                border:1px solid rgb(74 74 74);
+                border-radius: 5px;
+                outline: none;
+                resize: none;
+                background: transparent;
+                color: #fff;
+            }
+            label{
+                font-size: 1.2rem;
+                font-weight: 500;
+                margin-bottom: 0.5rem;
+                color: #fff;
+                opacity: 0.9;
+            }
+        }
+        .inner-input{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            border:2px dashed rgb(74 74 74);
+            border-radius: 5px;
+            padding: 1rem;
+            cursor: pointer;
+            height: 90px;
+            position: relative;
+            padding: 1rem;
+            .inner-label{
+                cursor: pointer;
+                margin: 0;
+                position: absolute;
+                top: 0;
+                width: 100%;
+                height: 100%;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                color: rgb(74 74 74);
+            }
+        }
+
+        .upload-btn{
+            display: flex;
+            justify-content: flex-end;
+            margin-top: 2rem;
+        }
+    }
 `;
 
 export default Upload
