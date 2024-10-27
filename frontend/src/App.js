21fb8248f59bd03901611441d75927db03a984d9
Kaan <kaanasutay10@gmail.com>
2024-10-27 23:20:10 +0300

    add videoPlayer styling and add video-info div

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,6 @@ function App() {
           <Route path="/" element={<Videos />} />
           <Route path="/videos/:id" element={<VideoPlayer />} />
         </Routes>
-        <Videos />
       </AppStyled>
     </BrowserRouter>
   );
